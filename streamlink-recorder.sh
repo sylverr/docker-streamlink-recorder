b62cccaf39c8a4be592be33ea922a6f20ec88367
sylverr <46232285+sylverr@users.noreply.github.com>
2022-04-05 19:34:22 +0200

    Update streamlink-recorder.sh

--- streamlink-recorder.sh
+++ streamlink-recorder.sh
@@ -3,6 +3,6 @@
 # For more information visit: https://github.com/downthecrop/TwitchVOD
 
 while [ true ]; do
-	streamlink $streamOptions $streamLink $streamQuality -o /home/download/{author}-{category}-{time:%Y%m%d-%H%M%S}.mkv
+	streamlink $streamOptions $streamLink $streamQuality -o /home/download/{author}-{url}-{time:%Y%m%d-%H%M%S}.mkv
 	sleep 60s
 done
